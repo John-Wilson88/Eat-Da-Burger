ae0f3fd97ef659263dd833d4d9eb4721d114e8f5
John <thehumantorch88@gmail.com>
2018-05-07 13:54:49 -0400

    heroku hosting aditions

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,25 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "root",
 	database: "burgers_db",
 	socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
+});*/
+
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burgers_db",
+		socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+ 	});
+};
 
 connection.connect(function(err){
 	if(err){
